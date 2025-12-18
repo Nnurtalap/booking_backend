7d9f9d3a6a9a8344f1f14bedb7cace2ff4b4fa03
nurtalap <orakovnurtalap@gmail.com>
2025-12-18 16:10:59 +0500

    Fix DATABASE_URL variable in database.py

--- app/database.py
+++ app/database.py
@@ -12,7 +12,7 @@ else:
     DATABASE_PARAMS = {}
 
 
-engine = create_async_engine(settings.DATABASE_URL, **DATABASE_PARAMS)
+engine = create_async_engine(DATABASE_URL, **DATABASE_PARAMS)
 
 
 async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
